6f3f5395bec2ac4bd35279926258d78db9cba3c3
Darlon Henrique <darlonhenrisouza@gmail.com>
2023-05-25 14:37:19 -0300

    feat: add update todo on database on change

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,20 @@
+"use client"
+
 type TodoItemProps = {
   id: string,
   title: string,
-  complete: boolean
+  complete: boolean,
+  toggleTodo: (id: string, complete: boolean) => void
 }
 
-export function TodoItem({ id, title, complete }: TodoItemProps) {
+export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
   
-
   return (
     <li className="flex gap-1 items-center">
-      <input id={id} type="checkbox" className="peer cursor-pointer" />
+      <input id={id} type="checkbox" className="peer cursor-pointer"
+      defaultChecked={complete}
+      onChange={e => toggleTodo(id, e.target.checked)}
+      />
       <label htmlFor={id} className="peer-checked:line-through cursor-pointer
       peer-checked:text-slate-500">{title}</label>
     </li>
