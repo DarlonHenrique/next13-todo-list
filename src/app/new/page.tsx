110bd64f1d258af921b998fe4d79ab0183844b92
Darlon Henrique <darlonhenrisouza@gmail.com>
2023-05-26 19:46:13 -0300

    fix: change the color of little calendar icon using pure css

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -33,8 +33,8 @@ export default function New() {
         <input type="text" name="title" className="border border-primary bg-transparent
           rounded px-2 py-1 outline-none focus-within:border-slate-100" />
         <label htmlFor="date">Due Date</label>
-        <input type="datetime-local" name="date" className="border border-primary bg-transparent
-          rounded px-2 py-1 outline-none focus-within:border-slate-100" />
+        <input type="datetime-local" name="date" className="datepicker-input border border-primary bg-transparent
+          rounded px-2 py-1 outline-none focus-within:border-slate-100 text-primary" />
         <div className="flex gap-1 justify-end">
           <Link className="border border-primary text-primary px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="..">Cancel</Link>
           <button className="border border-primary text-primary px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" type="submit">Create</button>
